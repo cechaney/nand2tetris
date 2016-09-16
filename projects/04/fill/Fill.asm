ca40613afc4d573a90de70c1abcc6013ff462a7d
Chris <chaneymail@gmail.com>
2016-09-16 01:31:58 -0500

    Completed Fill assignment

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -3,44 +3,86 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input. 
+// Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
 
-//Initialize the screen to white
-@WHITESCREEN
-0;JEQ
+//Initialize the current position pointer
+@8128
+D=A
+@CURRENT_POS
+M=D
 
-//Look for keypress
+
+//Initilize MAX screen position
+@8192
+D=A
+@MAX_POS
+M=D
+
+
+//Keypress detect loop
 (EVENTLOOP)
-
-//Load the Keyboard memory into A
 @KBD
 D=M
-
-//Darken screen if keypress D>0
-@BLACKSCREEN
+@BLACK
 D;JGT
-
-@WHITESCREEN
+@WHITE
 D;JEQ
-
-
-(BLACKSCREEN)
-@SCREEN
-M=1
-
 @EVENTLOOP
 0;JEQ
 
+(BLACK)
+@ARG
+M=-1
+@DRAW
+0;JEQ
 
-(WHITESCREEN)
-@SCREEN
+(WHITE)
+@ARG
 M=0
+@DRAW
+0;JEQ
 
+(DRAW)
+
+//Calculate the address to color
+@SCREEN
+D=A
+@CURRENT_POS
+A=D + M
+D=A
+@ADDRESS
+M=D
+
+//Set the color using ARG register value
+@ARG
+D=M
+@ADDRESS
+A=M
+M=D
+
+//Increment CURRENT_POS
+@CURRENT_POS
+M=M + 1
+
+@MAX_POS
+D=M
+@CURRENT_POS
+D=D - M
+@RESET
+D;JEQ
+
+//Jump back to event loop
+@EVENTLOOP
+0;JEQ
+
+(RESET)
+@CURRENT_POS
+M=0
 @EVENTLOOP
 0;JEQ
 
